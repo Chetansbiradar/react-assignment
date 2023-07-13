a6955719274da63d031f8d82c27c9f88a5ae4fed
Chetan P Hattoor <chetanhattoor@gmail.com>
2023-07-13 05:54:10 +0530

    empty cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Bill from './Bill';
 import ShopItemCard from './ShopItemCard';
 import { useSelector } from 'react-redux';
@@ -8,7 +9,7 @@ const Cart = () => {
   return (
     <div className='flex m-5'>
       <div className='flex-[80]'>
-        {items.length<=0 ? <h1>Cart is Empty</h1> : items.map((item)=><ShopItemCard key={item.id} item={item}/>)}
+        {items.length<=0 ? <div className='flex flex-col space-y-5 items-center'><h1>Cart is Empty</h1><Link to="/shopping" className='bg-slate-500 px-5 py-2 rounded-lg'>Go to Shop</Link></div> : items.map((item)=><ShopItemCard key={item.id} item={item}/>)}
       </div>
       <div className='flex-[20]'>
         <Bill items={items}/>
